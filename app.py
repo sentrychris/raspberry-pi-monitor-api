76b8b67c100ec08a4c57be735b846180981be874
cversyx <cmrowles@pm.me>
2019-07-15 18:40:59 +0100

    all

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, json, render_template
+import mon
 
 app = Flask(__name__)
 
@@ -6,9 +7,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/test/<var>')
-def test(var):
-    return 'Success' + var
+@app.route('/monitor')
+def monitor():
+    data = mon.get_system_info()
+    response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
